40bca911fa05b22750d765db4c0b6cd13c20201c
sem laan <semlaan@dds.nl>
2022-07-16 14:24:16 +0200

    Created a PlayerInput singleton that listens for player input and can relay it to other classes, added it to BeefyScene as a prefab

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private static PlayerInput _instance;
+    public static PlayerInput Instance { get { return _instance; } }
+
+    [HideInInspector] public Controls controls;
+    private bool wasInstance = false;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        controls = new Controls();
+        controls.Gameplay.LMouse.performed += _ => LeftMouseInput();
+        controls.Gameplay.RMouse.performed += _ => RightMouseInput();
+        controls.Gameplay.Enable();
+        wasInstance = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (wasInstance)
+        {
+            controls.Gameplay.LMouse.performed -= _ => LeftMouseInput();
+            controls.Gameplay.RMouse.performed -= _ => RightMouseInput();
+            controls.Gameplay.Disable();
+        }
+    }
+
+    public static void LeftMouseInput()
+    {
+    }
+
+    public static void RightMouseInput()
+    {
+    }
+}
