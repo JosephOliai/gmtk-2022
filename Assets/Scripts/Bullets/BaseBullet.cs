18f313449af7b4ad34ee2783db32cd6449fbb87d
sem laan <semlaan@dds.nl>
2022-07-17 13:23:49 +0200

    Bullets now get destroyed after some time

--- Assets/Scripts/Bullets/BaseBullet.cs
+++ Assets/Scripts/Bullets/BaseBullet.cs
@@ -7,6 +7,15 @@ public class BaseBullet : MonoBehaviour
     [SerializeField] private float speed = 1f;
     private Vector2 direction = Vector2.zero;
     private LayerMask target;
+    
+    // Timers for removing the bullet once its out of the screen
+    private float startTime = 0f;
+    protected float lifeTime = 5f;
+
+    private void Awake()
+    {
+        startTime = Time.time;
+    }
 
     public void Initialize(Vector2 direction, LayerMask target)
     {
@@ -16,7 +25,13 @@ public class BaseBullet : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Moving the bullet
         transform.position += (Vector3) direction * speed;
+        // Destroying the bullet once its out of life time (which is also used to remove bullets once they're probably off screen)
+        if (Time.time > (startTime + lifeTime))
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
